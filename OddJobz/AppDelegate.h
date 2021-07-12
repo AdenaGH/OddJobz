39a776693c74dd5dd87a145e612700f856c33c9f
AdenaGH <86276796+AdenaGH@users.noreply.github.com>
2021-07-12 12:03:01 -0400

    Initial Commit

---
+++ OddJobz/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  OddJobz
+//
+//  Created by Adena Rowana Ninvalle on 7/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ OddJobz/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  OddJobz
+//
+//  Created by Adena Rowana Ninvalle on 7/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
