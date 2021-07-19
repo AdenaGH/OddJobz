05bec36f9a26a2f004f36d5b04ca34eea8069059
AdenaGH <86276796+AdenaGH@users.noreply.github.com>
2021-07-19 18:29:27 -0400

    New View: Accepted Listings
    New view that shows listings that only the current user has accepted added

---
+++ OddJobz/View Controllers/AcceptedTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  AcceptedTableViewController.h
+//  OddJobz
+//
+//  Created by Adena Rowana Ninvalle on 7/19/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AcceptedTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
