403ae80c8852aad5cbcd926823c1ae3b9941d036
AdenaGH <86276796+AdenaGH@users.noreply.github.com>
2021-07-21 19:44:37 -0400

    Views
    -Home Table View now only shows listings from other people
    
    [IN THE BACKGROUND]
    New View added: AppliedTableViewController that will show a list of jobs that you applied to (to be added inside of a segment control)

---
+++ OddJobz/View Controllers/AppliedTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  AppliedTableViewController.h
+//  OddJobz
+//
+//  Created by Adena Rowana Ninvalle on 7/21/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AppliedTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- OddJobz/View Controllers/AppliedTableViewController.m
+++ OddJobz/View Controllers/AppliedTableViewController.m
@@ -1,15 +1,15 @@
 //
-//  HomeTableViewController.m
+//  AppliedTableViewController.m
 //  OddJobz
 //
-//  Created by Adena Rowana Ninvalle on 7/13/21.
+//  Created by Adena Rowana Ninvalle on 7/21/21.
 //
-
+/*
+#import "AppliedTableViewController.h"
 #import "HomeTableViewController.h"
 #import "Parse/Parse.h"
 #import "Listing.h"
 #import "ListingCell.h"
-#import "DetailsViewController.h"
 #import "LoginViewController.h"
 
 @interface HomeTableViewController ()  <UITableViewDelegate, UITableViewDataSource>
@@ -28,7 +28,7 @@
     self.tableView.delegate = self;
     
     
-    [self fetchListings];
+    //[self fetchListings];
     
     self.refreshCont = [[UIRefreshControl alloc] init];
     [self.refreshCont addTarget:self action:@selector(fetchListings) forControlEvents:UIControlEventValueChanged];
@@ -40,7 +40,6 @@
     PFQuery *query = [PFQuery queryWithClassName:@"Listing"];
     [query includeKey:@"poster"];
     //[query orderByDescending:@"createdAt"];
-    [query whereKey:@"poster" notEqualTo:[PFUser currentUser]];
 
 
 
@@ -48,7 +47,12 @@
     [query findObjectsInBackgroundWithBlock:^(NSArray *listings, NSError *error) {
         if (listings != nil) {
             // do something with the array of object returned by the call
-            self.listings = (NSMutableArray *) listings;
+            NSMutableArray * allListings = (NSMutableArray *) listings;
+            for(Listing *listing in allListings) {
+                if ([listing.applicants containsObject:[PFUser currentUser]])  {
+                    [self.listings addObject:listing];
+                }
+            }
             [self.tableView reloadData];
         } else {
             NSLog(@"%@", error.localizedDescription);
@@ -72,47 +76,29 @@
     return cell;
 }
 
-- (IBAction)logoutButtonPress:(id)sender {
-    NSLog(@"Logout action called");
-
-    //AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
-
-    
-    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-    //UITabBarController *tabController = [storyboard instantiateViewControllerWithIdentifier:@"TabBarController"];
-    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
-    //appDelegate.window.rootViewController = loginViewController;
-    
-    //[[UIApplication sharedApplication].keyWindow setRootViewController: tabController];
-    [[UIApplication sharedApplication].keyWindow setRootViewController: loginViewController];
-    loginViewController.modalPresentationStyle = UIModalPresentationFullScreen;
-    
-    [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
-        // PFUser.current() will now be nil
-    }];
-    
-    NSLog(@"Logout action finished");
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
 }
+*/
 
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
-    
-    if ([segue.identifier isEqual:@"detailsSegue"]) {
-        [self.tableView reloadData];
-        ListingCell *clickedCell = (ListingCell *)sender;
-        Listing *clickedListing = clickedCell.listing;
-        [self.tableView reloadData];
-        UINavigationController *nav = [segue destinationViewController];
-        DetailsViewController *detailsView = (DetailsViewController *) nav.topViewController;
-        detailsView.listing = clickedListing;
-        
-    }
-
 }
+*/
 
+//@end
 
-@end
